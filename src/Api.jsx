1d5e13dede6e560374b2f33d2619f119e761568f
surendra-1237 <dandurisurendra5@gmail.com>
2026-02-09 00:33:52 +0530

    add prod url

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://healthcare-be-q6pc.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
